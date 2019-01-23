b264a0a765605289cb1a7e5c1af81ab47f189c69
xavierBurgueno <xavierburgueno.xb@gmail.com>
2019-01-22 16:12:41 -0800

    Added Two Classes

---
+++ Source/TestingGrounds/ChooseNextWaypoint2.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWaypoint2 : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
